be244397246648a316e91093819e5e792fb7f834
Puneet Punj <puneetpunj88@gmail.com>
2022-07-04 15:58:56 +1000

    remove: tf files from main

--- provider.tf
+++
@@ -1,6 +0,0 @@
-# provider.tf
-
-# Specify the provider and access details
-provider "aws" {
-  region                  = var.aws_region
-}
--- variables.tf
+++
@@ -1,6 +0,0 @@
-# variables.tf
-
-variable "aws_region" {
-  description = "The AWS region things are created in"
-  default     = "ap-southeast-2"
-}
